0dcac9c715875d6826e34a2c4c0b9e46c4c1be2f
Shubham Agnihotri <shubham.agnihotri1997@gmail.com>
2020-10-11 12:53:46 +0530

    Update david_net.py

--- DeNN/custom_model/david_net.py
+++ DeNN/custom_model/david_net.py
@@ -2,68 +2,65 @@ from DeNN.base import BaseModel
 import torch.nn as nn
 import torch.nn.functional as F
 
+class Resnet_block(BaseModel):
+    def __init__(self,kernels, channels):
+        super(Resnet_block,self).__init__()
+        self.c1 = nn.Conv2d(kernels, channels, 3,padding = 1,bias=False)
+        self.b1 = nn.BatchNorm2d(channels)
+        self.re = nn.ReLU()
+        self.c2 = nn.Conv2d(channels, channels, 3,padding = 1,bias=False)
+        self.b2 = nn.BatchNorm2d(channels)
+        self.re2 = nn.ReLU()
+        self.residue = nn.Sequential()
+    
+    def forward(self,x):
+        out = self.re(self.b1(self.c1(x)))
+        out = self.re2(self.b2(self.c2(out)))
+        out = out + self.residue(out)
+        return out
+
+class Conv_block(BaseModel):
+    def __init__(self, kernels, channels):
+        super(Conv_block,self).__init__()
+        self.c1 = nn.Conv2d(kernels, channels, 3,padding = 1,bias=False)
+        self.mp = nn.MaxPool2d(2,2)
+        self.b1 = nn.BatchNorm2d(channels)
+        self.re = nn.ReLU()
+        self.resnet = Resnet_block(kernels,channels)
+    
+    def forward(self,x):
+        out = self.re(self.b1(self.mp(self.c1(x))))
+        res = self.resnet(out)
+        return out + res
+
+
+
 class David_net(BaseModel):
     def __init__(self):
         super(David_net, self).__init__()
         self.prep = nn.Sequential(
-            nn.Conv2d(3, 64, 3,padding = 1), 
+            nn.Conv2d(3, 64, 3,padding = 1,bias=False), 
             nn.BatchNorm2d(64),
             nn.ReLU()
         )
-        self.x = nn.Sequential(    
-            nn.Conv2d(64,128,3,padding = 1),
-            nn.MaxPool2d(2,2),
-            nn.ReLU(),
-        )
-        self.r1 = nn.Sequential(
-            nn.Conv2d(128, 128, 3,padding = 1), 
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, 3,padding = 1), 
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-        )
-        # self.layer1 = self.x + self.r1
-        self.layer2 =  nn.Sequential(
-            nn.Conv2d(128, 256, 3,padding = 1),
-            nn.MaxPool2d(2,2),
+        self.l1 = Conv_block(64,128)
+        self.l2 =  nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1,bias=False),
+            nn.MaxPool2d(2, 2),
             nn.BatchNorm2d(256),
             nn.ReLU()
         )
-        self.x2 = nn.Sequential(
-            nn.Conv2d( 256, 512,3,padding = 1),
-            nn.MaxPool2d(2,2),
-            nn.BatchNorm2d(512),
-            nn.ReLU()
-        )
-        self.r2 = nn.Sequential(
-            nn.Conv2d(512, 512, 3,padding = 1), 
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, 3,padding = 1), 
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-        )
-        # self.layer3 = self.x2 + self.r2
-        self.pool  = nn.Sequential(
-            nn.MaxPool2d(4,4)
-        )
-        self.fc = nn.Sequential(
-            nn.Conv2d(in_channels=512, out_channels=10, kernel_size=1, stride=1, bias=False)
-        )
-        self.stmax =nn.Softmax(dim=1)
+        self.l3 = Conv_block(256,512)
+        self.mp = nn.MaxPool2d(4,2)
+        self.fc = nn.Conv2d(512, 10, 1, bias=False)
+
     
     def forward(self,x):
-        out = self.prep(x)
-        x1 = self.x1(x)
-        r1 = self.r1(x1)
-        l1 = x1+r1
-        l2 = self.l2(l1)
-        x2 = self.x2(l2)
-        r2 = self.r1(x2)
-        l2 = x2+r2
-        p1 = self.pool(l2)
-        fc = self.fc(p1)
-        stmax = self.stmax(fc)
-        out = stmax.view(stmax.size(0), -1)
+        out = self.prep_layer(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.mp(out)
+        out = self.fc(out)
+        out = out.view(out.size(0), -1)
         return out
